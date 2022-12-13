ca7ee825654f96966aba6baf386010d62b243486
somsom13 <somin1113@naver.com>
2022-12-13 16:37:17 +0900

    feat(Course, Level): 과정, 레벨 enum 생성
    요구사항에 주어진 과정, 레벨, 미션값을 연관시켜 관리하기 위한 enum 클래스 생성

---
+++ src/main/java/pairmatching/domain/Course.java
@@ -0,0 +1,15 @@
+package pairmatching.domain;
+
+public enum Course {
+    BACKEND("백엔드"),
+    FRONTEND("프론트엔드");
+
+    private String name;
+
+    Course(String name) {
+        this.name = name;
+    }
+
+    // 코스 이름 조회
+    // 입력값 검증
+}
---
+++ src/main/java/pairmatching/domain/Level.java
@@ -0,0 +1,24 @@
+package pairmatching.domain;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public enum Level {
+    LEVEL1("레벨1", Arrays.asList("자동차경주", "로또", "숫자야구게임")),
+    LEVEL2("레벨2", Arrays.asList("장바구니", "결제", "지하철노선도")),
+    LEVEL3("레벨3", Collections.emptyList()),
+    LEVEL4("레벨4", Arrays.asList("성능개선", "배포")),
+    LEVEL5("레벨5", Collections.emptyList());
+
+    private String name;
+    private List<String> missions;
+
+    Level(String name, List<String> missions) {
+        this.name = name;
+        this.missions = missions;
+    }
+
+    // 입력값 검증
+    // 메뉴 조회
+}
